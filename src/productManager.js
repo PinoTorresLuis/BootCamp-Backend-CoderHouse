0034f3129ecbcb38f31cce4e660158d9b6465161
Pino Luis <pinotorres.luis@gmail.com>
2023-08-09 14:36:07 -0300

    se agregaron carpetas/archivos de la clase 08

--- src/productManager.js
+++ src/productManager.js
@@ -4,6 +4,26 @@ export class ProductManager{
     constructor(path){  //utilizo el path para que funcione como parámetro en mis métodos así al momento de exportar la clase en app.js y al instanciarla poder pasar la ruta del archivo productos.json 
         this.path = path;
     }
+	addProduct = async (product) => {
+		const products = JSON.parse (await fs.readFile(this.path,'utf-8'));
+   
+		  //Código para verificar que no se repita el mismo código de producto
+		if (products.some(prod => prod.code === product.code)) {
+		  console.log(`ERROR: Ya existe un producto con el código:${product.code}`)
+		  return
+		  }
+  
+		if( !product.title || !product.description || !product.price || !product.thumbnail ||! product.code || product.stock < 0){
+			console.log("Todos los campos son obligatorios")
+			return
+		  }else{
+			products.push(product) //Se utiliza push para pasar los datos que se encuentran al array vacio en el archivo .txt
+		  }
+  
+		await fs.writeFile(this.path,JSON.stringify(products));
+		 
+		console.log ("Producto agregado correctamente");
+	}
 
 	async getProducts() {
 		const products = JSON.parse(await fs.readFile(this.path, 'utf-8'));
@@ -14,7 +34,36 @@ export class ProductManager{
 		const products = JSON.parse(await fs.readFile(this.path, 'utf-8'));
 		return products.find(product => product.id == parseInt(id)) ?? console.log('Not Found');
 	}
-	
+	   //Método para actualizar los productos en el archivo .txt
+	async updateProducts(id, {title, description, price, thumbnail, code, stock }){
+        const products = JSON.parse (await fs.readFile(this.path, 'utf-8'))
+        const index = products.findIndex (prod => prod.id === id)
+
+        if (index !== -1){ 
+            const product = products[index]
+            product.title = title ?? product.title;
+            product.description = description ?? product.description;
+            product.price = price ?? product.price;
+            product.thumbnail = thumbnail ?? product.thumbnail;
+            product.code = code ?? product.code;
+            product.stock = stock ?? product.stock;
+        
+            await fs.writeFile(this.path, JSON.stringify(products));
+            console.log(`Tu producto se actualizó correctamente!`, JSON.stringify(products[index]))
+        }else{
+            console.log (`El producto con el ID: ${id} no existe!`)
+        }
+    }
+    //Método para borrar los productos según el ID ingresado
+    async deleteProducts(id){
+        const products = JSON.parse (await fs.readFile(this.path,'utf-8'))
+        const deleteID = products.filter(prod => prod.id != id)
+
+        await fs.writeFile(this.path, JSON.stringify(deleteID));
+         
+        console.log ("Producto borrado correctamente");    
+      }
+
 }
 
 
---
+++ src/routes/products.routes.js
@@ -0,0 +1,50 @@
+import { Router } from "express";
+import { ProductManager } from "../productManager.js";
+
+const routerProd = Router();
+const productManager = new ProductManager ('src/models/productos.txt');
+
+routerProd.get ('/', async(req,res)=>{
+    const {limit} = req.query;
+    const productos = await productManager.getProducts();
+    const prods = productos.slice(0,limit);
+    res.status(200).send(prods);
+});
+
+routerProd.get ('/:id', async(req,res)=>{
+    const {id} = req.params
+    const prods = await productManager.getProductById(parseInt(id));
+    if (prods){
+        res.status(200).send(prods);
+    }else {
+        res.status(404).send("Producto no encontrado")
+    }
+});
+
+routerProd.post ('/', async(req,res)=>{
+    const confirmacion = await ProductManager.addProducts(req.body);
+    if(confirmacion){
+        res.status(200).send("Producto creado correctamente");
+    }else {
+        res.status(404).send("Producto ya existente")
+    }
+});
+
+routerProd.delete ('/:id', async(req,res)=>{
+    const confirmacion = await ProductManager.deleteProduct(req.params.id);
+    if(confirmacion){
+        res.status(200).send("Producto borrado correctamente");
+    }else {
+        res.status(400).send("Producto no encontrado")
+    }
+});
+routerProd.put ('/:id',  async(req,res)=>{
+    const confirmacion = await ProductManager.updateProduct(req.params.id, req.body);
+    if(confirmacion){
+        res.status(200).send("Producto actualizado correctamente");
+    }else {
+        res.status(404).send("Producto no encontrado")
+    }
+});
+
+export default routerProd;
