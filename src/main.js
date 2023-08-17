b23b0d1056da43cd0735252c44096713322dfc94
Pino Luis <pinotorres.luis@gmail.com>
2023-08-17 14:45:03 -0300

    se está trabajando en webpack y socket io

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import path from 'path';
+import { Server } from 'socket.io';
 import { engine } from 'express-handlebars'
-
 import {__dirname} from './path.js';
+
 import routerProds from './routes/products.routes.js';
 import cartRouter from './routes/cart.routes.js';
 
+
 const PORT = 4000;
 const app = express();
+//Server
+//Esto se hace porque socket io necesita saber la configuración de los servidores
+const server = app.listen(PORT,()=>{
+    console.log("SERVIDOR FUNCIONANDO EN PUERTO:", PORT);
+})
+const io = new Server(server);
 
 //Middlewares
 app.use(express.json());
@@ -16,13 +24,52 @@ app.engine('handlebars', engine())//Defino que voy a trabajar con handlebars
 app.set('view engine', 'handlebars');//Defino extensión
 app.set('views', path.resolve(__dirname, './views')) //Defino localización
 
+//Conexión de Socket.io
+io.on("connection", (socket)=>{
+    console.log("Conexión con Socket.io");
+
+    socket.on('newProduct',(prod)=>{
+        console.log(prod);
+        //Debería agregarse al txt mediante addProduct
+    } )
+
+
+  /*   socket.on('mensaje', info =>{
+        console.log(info);
+        socket.emit('respuesta', false);
+    })
+
+    socket.on('juego', (infoJuego)=>{
+        if(infoJuego == 'truco'){
+            console.log("Conexión a Poker exitosa")
+        } else {
+            console.log("Conexión a Truco exitosa")
+        }
+    })
+ */
+
+})
+
 //Routes
 app.use('/static', express.static(path.join(__dirname,'/public')));
 app.use ('/api/product', routerProds);
 app.use('/api/cart/',cartRouter);
+
 //HBS
 app.get ('/static', (req,res)=>{
-    const user = {
+
+    res.render("realTimeProducts",{
+        titulo: "RealTimeProducts",
+        rutaJS: "realTimeProducts.js",
+        rutaCSS: "style.css"
+    })
+
+
+})
+
+  /*
+  Código de HBS 
+  const user = {
         nombre: "Luis",
         cargo: "Tutor"
     }
@@ -33,13 +80,13 @@ app.get ('/static', (req,res)=>{
     ]
 
 //Indicar que plantilla voy a utilizar
-res.render('users',{
-    titulo:"Users",
+res.render('realt',{
+    titulo:"realt",
     usuario: user,
     isTutor: user.cargo === "Tutor",
     cursos:cursos
-})
-})
+}) */
+
 
 
 
@@ -47,8 +94,4 @@ app.get('/*',(req,res)=>{
     res.send("Error 404: Page not found");
 })
 
-//Server
-app.listen(PORT,()=>{
-    console.log("SERVIDOR FUNCIONANDO EN PUERTO:", PORT);
-})
 
---
+++ src/public/js/realTimeProducts.js
@@ -0,0 +1,11 @@
+const socket = io ();
+
+const form = document.getElementById("formProduct");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const datForm = new FormData (e.target);
+    const prod = Object.fromEntries(datForm);
+    socket.emit('newProduct',prod);
+    console.log(prod)
+} )
