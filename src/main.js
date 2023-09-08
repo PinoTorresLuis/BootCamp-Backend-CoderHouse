a69699f08fe056d7402f4d40cc30072dc2d7bfe0
Pino Luis <pinotorres.luis@gmail.com>
2023-09-08 15:05:58 -0300

    se actualizo la ruta de mongoDB

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import mongoose from 'mongoose';
+import mongoConnect from './database.js';
 
 import path from 'path';
 import {__dirname} from './path.js';
 
 import { Server } from 'socket.io';
 import { engine } from 'express-handlebars'
+
+
 
 /*    Rutas que ya no se están usando
 import routerProds from './routes/products.routes.js';
@@ -29,7 +31,7 @@ import {orderModel} from './models/order.models.js';
 
 const PORT = 4000;
 const app = express();
-
+mongoConnect();
 //Server
 //Se ubica acá arriba porque Socket io necesita saber la configuración de los servidores
 const server = app.listen(PORT,()=>{
@@ -61,50 +63,7 @@ app.get('/*',(req,res)=>{   //Ruta con error 404 que se utiliza a nivel general
     res.send("Error 404: Page not found");
 })
 
-mongoose.connect('mongodb+srv://luispino:coderhouse@clustercoder.rrpt8ts.mongodb.net/?retryWrites=true&w=majority')
-.then(async()=>{
-  console.log("DB conectada");
-  /*await orderModel.create([ 
-  const resultados = await orderModel.paginate({size:'medium'}, {limit:1})
-  console.log(resultados);
-    {name:'Muzza', size:'large',price:10,quantity:5},
-    {name:'Peperoni', size:'medium',price:100,quantity:2},
-    {name:'Salchi', size:'small',price:400,quantity:4},
-    {name:'Jamon crudo', size:'large',price:700,quantity:8},
-    {name:'Roquefort', size:'small',price:80,quantity:10},
-    {name:'Especial', size:'medium',price:80,quantity:10}, 
-  
-  const resultados = await orderModel.aggregate([
-    {
-      $match:{ size:'medium'}
-    },
-    {
-      $group: {_id:"$name", totalQuantity:{$sum:"$quantity"},
-    totalPrice : {$sum :"$price"}}
-    },
-    {
-      $sort: {totalPrice: -1} //1 mayor a menor, -1 es para ir de menor a mayor.
-    },
-    {
-      $group : {_id:1, orders:{$push: "$$ROOT"}} //ROOT significa estado actual de la agregación
-    },
-      {
-      $project: { //Genero un nuevo proyecto con los datos previos
-      "_id": 0,
-      orders:"$orders"
-    }
-  },
-  {
-    $merge: {
-      into: "reports" //Guardo en la colección en la nube los reportes
-    }
-  }
-  ])
-  console.log(resultados);
-  ]*/
-})
 
-.catch((e)=>console.log("Error en conexión a MONGO DB Atlas", e));
 
 //Conexión de Socket.io
 io.on("connection", (socket)=>{
